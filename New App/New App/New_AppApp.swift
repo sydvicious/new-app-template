9e5c0cb5a6fcb894b04a21259566b09444dc61a3
Syd Polk <syd@sydpolk.com>
2025-01-06 17:31:55 -0600

    Step 1: Default Xcode project.

---
+++ New App/New App/New_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_AppApp.swift
+//  New App
+//
+//  Created by Syd Polk on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct New_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ New App/New AppTests/New_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  New_AppTests.swift
+//  New AppTests
+//
+//  Created by Syd Polk on 1/6/25.
+//
+
+import Testing
+@testable import New_App
+
+struct New_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
