3c19e13ee8b05b3cb291c39ef8f6d81304618735
Syd Polk <syd@sydpolk.com>
2025-01-06 17:55:33 -0600

    Step 1: Text needs to be retrieved somehow.

--- New App/New App/ContentView.swift
+++ New App/New App/ContentView.swift
@@ -13,9 +13,13 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(getText())")
         }
         .padding()
+    }
+    
+    func getText() -> String {
+        return "Hello, world!"
     }
 }
 
